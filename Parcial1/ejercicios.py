45c3f933da40755fa18606280dcaf4bbf97f3660
ManuelBecerra <becerramanu21@gmail.com>
2023-09-06 18:24:19 -0500

    Parcial

--- Parcial1/ejercicios.py
+++ Parcial1/ejercicios.py
@@ -11,6 +11,7 @@ def muller(f, x0, x1, x2, tol):
     c=f(x0)-(x0*fx0x1)+(x0*x1*a)
     h=x2-x1
     i=0
+    
     while abs(h) > tol:
         D = (b ** 2 - (4 * a * c)) ** 0.5
         if abs(b - D) > abs(b + D):
@@ -27,16 +28,26 @@ def muller(f, x0, x1, x2, tol):
         b=fx0x1-(x0+x1)*a
         c=f(x0)-(x0*fx0x1)+(x0*x1*a)
         h=x2-x1
-        i+=1
-        if i==100:
-           print("no se logro copilar :(")
-           break
-    return y,i,D
+        
+        if abs(y) < tol:
+            break
+        
 
+        if i==100:
+            print("No se pudo")
+        i+=1
+    return y,i,D
+            
+            
+
+#Punto B y C
+print("Nuestro x_0 es -1, nuestro x_1 es 1 y nuestro x_2 es 0")
 root = muller(funcion, -1,1, 0, 1*10**(-10))
-print('la raiz de la función es '+str(root[0]))
-print('El numero de iteraciones fueron '+str(root[1]))
-print('El valor de X3 es '+str(root[0]))
-#punto B
+
+print("La raiz es "+str(root[0])+" numero de iteraciones es "+str(root[1])+" x_3 es "+str(root[2]))
+
+
+
+
 
 
