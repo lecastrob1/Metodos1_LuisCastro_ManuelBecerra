878a62610ab72e5aabcbb83601ca625b2cc22e4b
ManuelBecerra <becerramanu21@gmail.com>
2023-09-07 19:40:42 -0500

    actualizar

--- Taller_1/expansiontermica_olivina_grafito.py
+++ Taller_1/expansiontermica_olivina_grafito.py
@@ -5,12 +5,12 @@ archivo1 = 'olivine_angel_2017.csv'
 olivine = ExpansionTermicaMineral(archivo1)
 print("El error global del olivine es "+str(olivine.calcular_y_graficar()[0]))
 print("El coeficiente de expansion del olivine es "+str(olivine.calcular_y_graficar()[1]))
-olivine.calcular_y_graficar()
-plt.show
+print("Y esta es las graficas de Olivine")
+plt.show()
 
 archivo2 = 'graphite_mceligot_2016.csv'
 graphite = ExpansionTermicaMineral(archivo2)
 print("El error global del grafito es "+str(graphite.calcular_y_graficar()[0]))
 print("El coeficiente de expansion del grafito es "+str(graphite.calcular_y_graficar()[1]))
-graphite.calcular_y_graficar()
-plt.show
+print("Y esta es las graficas de Grafito")
+plt.show()
